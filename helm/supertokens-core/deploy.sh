149293c535df43d6cced1ae66a91672eeea19489
Max Morhardt <70174942+maxmorhardt@users.noreply.github.com>
2024-04-02 12:50:20 -0400

    added supertokens deploy script

---
+++ helm/supertokens-core/deploy.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+export APP_NAME=supertokens-core
+export APP_VERSION=1.0.0
+
+# Need DOCKER_USERNAME and DOCKER_PASSWORD env vars
+helm registry login 'registry-1.docker.io' --username "$DOCKER_USERNAME" --password "$DOCKER_PASSWORD"
+helm package .
+helm push ./$APP_NAME-$APP_VERSION.tgz oci://registry-1.docker.io/$DOCKER_USERNAME
+helm upgrade $APP_NAME ./$APP_NAME-$APP_VERSION.tgz --install --atomic --debug --history-max=3 -n topfilms
