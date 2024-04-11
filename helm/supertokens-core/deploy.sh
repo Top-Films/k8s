8ece93bd9acb6be6ff4bcb169250b3e0bc4fcfa4
Max Morhardt <70174942+maxmorhardt@users.noreply.github.com>
2024-04-11 19:45:25 -0400

    added comments/redis skeleton

--- helm/supertokens-core/deploy.sh
+++ helm/supertokens-core/deploy.sh
@@ -3,6 +3,7 @@ export APP_NAME=supertokens-core
 export APP_VERSION=1.0.0
 
 # Need DOCKER_USERNAME and DOCKER_PASSWORD env vars
+# Needs secret manifest to be applied
 helm registry login 'registry-1.docker.io' --username "$DOCKER_USERNAME" --password "$DOCKER_PASSWORD"
 helm package .
 helm push ./$APP_NAME-$APP_VERSION.tgz oci://registry-1.docker.io/$DOCKER_USERNAME
