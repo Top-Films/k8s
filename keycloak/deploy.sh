fc22b099c9baa81be0a39ddb52c4282f95dff073
Max Morhardt <70174942+maxmorhardt@users.noreply.github.com>
2024-07-29 15:06:56 -0400

    fix deploys for jenkins

--- keycloak/deploy.sh
+++ keycloak/deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 # kubectl create secret tls auth.topfilms.io-tls --cert=cert.pem --key=key.pem -n keycloak
-kubectl apply -f secret.yaml -n keycloak
+# kubectl apply -f secret.yaml -n keycloak
 kubectl apply -f keycloak.yaml -n keycloak
--- keycloak/undeploy.sh
+++ keycloak/undeploy.sh
@@ -3,5 +3,5 @@
 kubectl delete deploy keycloak -n keycloak
 kubectl delete svc keycloak -n keycloak
 kubectl delete ing keycloak -n keycloak
-kubectl delete secret keycloak-credentials -n keycloak
+# kubectl delete secret keycloak-credentials -n keycloak
 # kubectl delete secret auth.topfilms.io-tls -n keycloak
