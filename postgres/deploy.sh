6311165a1ab831056cb763102e49a2defc6f4ef1
Max Morhardt <70174942+maxmorhardt@users.noreply.github.com>
2024-08-08 21:37:19 -0400

    add namespace to scripts

--- postgres/deploy.sh
+++ postgres/deploy.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+# k create namespace postgres
 kubectl apply -f secret.yaml -n postgres
 kubectl apply -f storage.yaml -n postgres
 helm install db oci://registry-1.docker.io/bitnamicharts/postgresql --namespace postgres -f values.yaml
