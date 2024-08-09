0a53afa7e5853113ce4e2a29155a7d82257fa3b2
Max Morhardt <70174942+maxmorhardt@users.noreply.github.com>
2024-08-08 21:46:20 -0400

    add commands to jenkins

--- jenkins/deploy.sh
+++ jenkins/deploy.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
+# k create namespace keycloak
 # helm repo add jenkins https://charts.jenkins.io
+# helm repo update
 # kubectl create secret tls jenkins.topfilms.io-tls --cert=cert.pem --key=key.pem -n jenkins
 kubectl apply -f storage.yaml -n jenkins
 kubectl apply -f sa.yaml -n jenkins
