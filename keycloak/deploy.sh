afa102a7a5ee01c14b92c097089f614acc844be6
Max Morhardt <70174942+maxmorhardt@users.noreply.github.com>
2024-08-12 18:49:00 -0400

    comment out keycloak

--- keycloak/deploy.sh
+++ keycloak/deploy.sh
@@ -2,7 +2,7 @@
 
 # k create namespace keycloak
 # kubectl create secret tls auth.topfilms.io-tls --cert=cert.pem --key=key.pem -n keycloak
-kubectl apply -f secret.yaml -n keycloak
+# kubectl apply -f secret.yaml -n keycloak
 echo "$DOCKER_PASSWORD" | helm registry login registry-1.docker.io --username $DOCKER_USERNAME --password-stdin
 helm package helm --app-version=23.0.7 --version=23.0.7
 helm push ./keycloak-23.0.7.tgz oci://registry-1.docker.io/$DOCKER_USERNAME
