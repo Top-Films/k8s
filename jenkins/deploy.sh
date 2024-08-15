8a03409cfcdaee0508d8277ba0f7ad95cf333fe3
Max Morhardt <70174942+maxmorhardt@users.noreply.github.com>
2024-08-15 13:35:48 -0400

    update jenkins depl

--- jenkins/deploy.sh
+++ jenkins/deploy.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 # k create namespace keycloak
-# helm repo add jenkins https://charts.jenkins.io
-# helm repo update
 # kubectl create secret tls jenkins.topfilms.io-tls --cert=cert.pem --key=key.pem -n jenkins
-kubectl apply -f storage.yaml -n jenkins
-kubectl apply -f sa.yaml -n jenkins
-helm install jenkins -n jenkins -f values-with-secrets.yaml jenkinsci/jenkins
+# kubectl apply -f storage.yaml -n jenkins
+# kubectl apply -f sa.yaml -n jenkins
+helm repo add jenkins https://charts.jenkins.io
+helm repo update
+helm upgrade jenkins jenkinsci/jenkins -f values-with-secrets.yaml --install --atomic --debug --history-max=3 --namespace jenkins
