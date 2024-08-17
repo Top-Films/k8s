469dbb7cb512be2b81b439b4925406f81207cf84
Max Morhardt <70174942+maxmorhardt@users.noreply.github.com>
2024-08-17 14:02:20 -0400

    create jenkins pipeline

--- jenkins/deploy.sh
+++ jenkins/deploy.sh
@@ -1,9 +1,17 @@
 #!/bin/bash
 
-# k create namespace keycloak
-# kubectl create secret tls jenkins.topfilms.io-tls --cert=cert.pem --key=key.pem -n jenkins
-# kubectl apply -f storage.yaml -n jenkins
-# kubectl apply -f sa.yaml -n jenkins
+# sed -i 's/<JENKINS_ADMIN_USERNAME>/$JENKINS_ADMIN_USERNAME/g' values.yaml
+# sed -i 's/<JENKINS_ADMIN_PASSWORD>/$JENKINS_ADMIN_PASSWORD/g' values.yaml
+# sed -i 's/<JENKINS_CLIENT_ID>/$JENKINS_CLIENT_ID/g' values.yaml
+# sed -i 's/<JENKINS_CLIENT_SECRET>/$JENKINS_CLIENT_SECRET/g' values.yaml
+
+kubectl create namespace jenkins
+
+kubectl create secret tls jenkins.topfilms.io-tls --cert=cert.pem --key=key.pem -n jenkins
+
+kubectl apply -f storage.yaml -n jenkins
+kubectl apply -f sa.yaml -n jenkins
+
 helm repo add jenkins https://charts.jenkins.io
 helm repo update
-helm upgrade jenkins jenkinsci/jenkins -f values-with-secrets.yaml --install --atomic --debug --history-max=3 --namespace jenkins
+helm upgrade jenkins jenkinsci/jenkins -f values.yaml --install --atomic --debug --history-max=3 --namespace jenkins
