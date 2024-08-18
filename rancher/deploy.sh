1c0fd02af297759138812a8a1e6a02193120b1e9
Max Morhardt <70174942+maxmorhardt@users.noreply.github.com>
2024-08-18 19:43:01 -0400

    rancher jenkins file

--- rancher/deploy.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-kubectl create namespace cattle-system
-kubectl create secret tls rancher.topfilms.io-tls --cert=cert.pem --key=key.pem -n cattle-system
-
-helm repo add rancher-latest https://releases.rancher.com/server-charts/latest
-helm repo update
-
-helm install rancher rancher-latest/rancher -f values.yaml -n cattle-system
