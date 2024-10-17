e53bbd6fed2c1296be536bcc8909021aefb220e0
Max Morhardt <70174942+maxmorhardt@users.noreply.github.com>
2024-10-16 22:50:23 -0400

    comments and improvements

--- jenkins/jobs/scrape-movies/scrape-movies.py
+++ jenkins/jobs/scrape-movies/scrape-movies.py
@@ -1,30 +1,35 @@
 from selenium import webdriver  
 from selenium.webdriver.common.by import By
+from selenium.common.exceptions import NoSuchElementException
 import psycopg2
+import logging
 import os
 import datetime
-import uuid
 import time
-import logging
+import uuid
 
 log = logging.getLogger(__name__)
 logging.basicConfig(level=logging.INFO)
 
 class ScrapeMovies:
 	def __init__(self, db_name, db_host, db_username, db_password, db_port, init_genres_table):
+		# db conn
 		self.conn = psycopg2.connect(database=db_name,
 									 host=db_host,
 									 user=db_username,
 									 password=db_password,
 									 port=db_port)
 		
-		self.init_genres_table = init_genres_table
-		self.jenkinsUserId = '4308b779-f616-4ada-9ac8-4ddb27bcd749' # srv-jenkins id
+		# jenkins flag to init genres table
+		self.init_genres_table_flag = init_genres_table
 
+		# constants
+		self.jenkinsUserId = '4308b779-f616-4ada-9ac8-4ddb27bcd749' # srv-jenkins id
 		self.base_url = r'https://www.allmovie.com/genre'
 		self.query = 'alltime-desc'
 		self.num_movies_per_page = 20
 		self.page_offset = 1
+		self.max_retries = 5
 		
 		# self.genres = [
 		# 	['action-adventure-ag100', 'Action Adventure', '97128c0e-c0e9-4c0c-93bd-fdb5f7bf2c3c'],
@@ -62,12 +67,15 @@ class ScrapeMovies:
 
 
 	def init_genres_table(self):
-		log.info(f"Initializing data in MOVIE_GENRE table\n")
+		# insert data into genres table
+		log.info(f"Initializing data in MOVIE_GENRE table")
 		for genre in self.genres:
+			# parse genre
 			name = genre[1]
 			id = genre[2]
 			timestamp = datetime.datetime.now()
 
+			# log data
 			log.info(f"Creating record:")
 			log.info(f"created_by={self.jenkinsUserId}")
 			log.info(f"updated_by={self.jenkinsUserId}")
@@ -76,24 +84,32 @@ class ScrapeMovies:
 			log.info(f"id={id}")
 			log.info(f"name={name}\n")
 
-			self.conn.cursor().execute('INSERT INTO MOVIE_GENRE (id, created_by, updated_by, created_at, updated_at, name) VALUES (%s, %s, %s, %s, %s, %s)', (id, self.jenkinsUserId, self.jenkinsUserId, timestamp, timestamp, name))
+			# execute insert
+			cursor = self.conn.cursor()
+			cursor.execute('INSERT INTO MOVIE_GENRE (id, created_by, updated_by, created_at, updated_at, name) VALUES (%s, %s, %s, %s, %s, %s)', (id, self.jenkinsUserId, self.jenkinsUserId, timestamp, timestamp, name))
 		
+		# finish transaction after all inserts
 		self.conn.commit()
-		log.info(f"Successfully initialized data in MOVIE_GENRE table \n")
+		cursor.close()
+		log.info(f"Successfully initialized data in MOVIE_GENRE table\n")
 
 	def scrape(self):
+		# potential for threading if no rate limits
 		for genre in self.genres:
 			self.__scrape_genre(genre)
 			
 	def __scrape_genre(self, genre):
+		# parse values from genre
 		genre_url_path = genre[0]
 		genre_name = genre[1]
 		genre_id = genre[2]
 
+		
+
+		# continue until user exceeds 5 attempts to parse a page or end of genre
+		continue_genre = True
 		# page_num = 1
 		page_num = 2344
-
-		continue_genre = True
 		while continue_genre:
 			url = f"{self.base_url}/{genre_url_path}/{self.query}/{page_num}"
 			continue_genre = self.__scrape_page(url, page_num, genre_name, genre_id)
@@ -102,45 +118,48 @@ class ScrapeMovies:
 					
 	def __scrape_page(self, url, page_num, genre_name, genre_id) -> bool:
 		start_time = time.time()
-
 		log.info(f"-------------------- {genre_name}: {page_num} --------------------")
-
+		# attempt to parse page 5 times
 		attempt_count = 1
-		max_retries = 5
-
-		completed_scrape = False
-		while completed_scrape == False and attempt_count <= max_retries:
-			log.info(f"Attempt {attempt_count}/{max_retries}: {url}")
+		while attempt_count <= self.max_retries:
+			log.info(f"Attempt {attempt_count}/{self.max_retries}: {url}")
 			try:
+				# get driver and url
 				driver = self.__init_driver()
 				driver.get(url)
 
+				# attempt to parse all 20 movies on a page
 				for movie_num in range(self.page_offset, self.num_movies_per_page + self.page_offset):
 					self.__scrape_movie(driver, movie_num, genre_id)
 
+				# page complete with more movies within the genre
 				driver.close()
-
-				# self.conn.commit()
-
-				completed_scrape = True
-
 				end_time = time.time()
 				log.info(f"Successfully scraped {genre_name} ({page_num}): {round(end_time-start_time, 2)}s\n")
-
+				return True
+			except NoSuchElementException:
+				# genre is complete
+				driver.quit()
+				log.info(f"Successfully reached end of genre {genre_name} ({page_num}): {url}\n")
 				return False
 			except Exception as e:
+				# failed to parse page for unknown reason
 				log.error(f"Error on attempt {attempt_count}: {url}")
 				log.error(e)
 
+				# increase count
 				attempt_count = attempt_count + 1
 
+				# re init driver fully
 				driver.quit()
 				driver = self.__init_driver()
 
-		log.error(f"Maximum attempts reached: url={url} | genre={genre_name} | page_num={page_num}\n")
+		# failed to parse 5 times
+		log.warning(f"Maximum attempts reached: url={url} | genre={genre_name} | page_num={page_num}\n")
 		return False
 	
 	def __init_driver(self):
+		# init chrome driver for chromium browser arm64 linux
 		options = webdriver.ChromeOptions()
 		options.add_argument('--headless=new')
 		options.add_argument("--no-sandbox")
@@ -153,27 +172,43 @@ class ScrapeMovies:
 		return driver
 	
 	def __scrape_movie(self, driver, movie_num, genre_id):
+		# get wrapper element
+		movie_wrapper_elem = None
 		movie_wrapper_elem = driver.find_element(By.CLASS_NAME, f"num-{movie_num}") 
-
+		
+		# parse title
 		title_wrapper_elem = movie_wrapper_elem.find_element(By.CLASS_NAME, 'title')
 		title_elem = title_wrapper_elem.find_element(By.TAG_NAME, 'a')
 		title = title_elem.text
 
+		# parse title
 		directors_wrapper_elem = movie_wrapper_elem.find_element(By.CLASS_NAME, 'directors')
 		directors_elem = directors_wrapper_elem.find_element(By.TAG_NAME, 'a')
 		director = directors_elem.text
 
+		# parse year
 		year_elem = movie_wrapper_elem.find_element(By.CLASS_NAME, 'movie-year')
 		year = year_elem.text
 
+		# ensure movie does not already exist
+		cursor = self.conn.cursor()
+		cursor.execute('SELECT * FROM MOVIE WHERE name = %s AND year = %d', (title, year))
+		if len(cursor.fetchall()) > 0:
+			# ignore duplicate record
+			log.info(f"Movie already exists - {movie_num}: title={title} | director={director}")
+			cursor.close()
+			return
+
+		# create new record
 		timestamp = datetime.datetime.now()
 		id = str(uuid.uuid4())
-
 		log.info(f"{movie_num}: title={title} | director={director} | year={year} | time={timestamp} | id={id}")
-		self.conn.cursor().execute('INSERT INTO MOVIE (id, created_by, updated_by, created_at, updated_at, name, director, movie_genre_id, year) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s)', (id, self.jenkinsUserId, self.jenkinsUserId, timestamp, timestamp, title, director, genre_id, year))
-	
+		cursor.execute('INSERT INTO MOVIE (id, created_by, updated_by, created_at, updated_at, name, director, movie_genre_id, year) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s)', (id, self.jenkinsUserId, self.jenkinsUserId, timestamp, timestamp, title, director, genre_id, year))
+		self.conn.commit()
+		cursor.close()
+
 	def main(self):
-		if (self.init_genres_table == 'true'):
+		if (self.init_genres_table_flag == 'true'):
 			self.init_genres_table()
 		
 		self.scrape()
