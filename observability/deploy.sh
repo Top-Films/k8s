79eaa868dac5d9c0b06548d2191ec4d3741eb70e
Max Morhardt <70174942+maxmorhardt@users.noreply.github.com>
2024-10-28 17:12:49 -0400

    final deploy changes

--- observability/deploy.sh
+++ observability/deploy.sh
@@ -1,9 +1,31 @@
-# helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
-# helm repo update
-# helm pull prometheus-community/prometheus --untar
-# helm install prometheus prometheus-community/prometheus --namespace observability
+export GRAFANA_CLIENT_ID=""
+export GRAFANA_CLIENT_SECRET=""
+export CA_CERT_PATH=""
+export CA_CERT_PRIVATE_KEY_PATH=""
+
+if [[ -z "$GRAFANA_CLIENT_ID" || -z "$GRAFANA_CLIENT_SECRET" || -z "$CA_CERT_PATH" || -z "$CA_CERT_PRIVATE_KEY_PATH" ]]; then
+	echo "ERROR: Environment variables not set"
+	exit 1
+fi
+
+sed -i "s/<GRAFANA_CLIENT_ID>/$GRAFANA_CLIENT_ID/g" grafana-values.yaml
+sed -i "s/<GRAFANA_CLIENT_SECRET>/$GRAFANA_CLIENT_SECRET/g" grafana-values.yaml
+
+cp $CA_CERT_PATH .
+cp $CA_CERT_PRIVATE_KEY_PATH .
+
+kubectl delete secret grafana.topfilms.io-tls --namespace observability
+kubectl create secret tls grafana.topfilms.io-tls --cert=cert.pem --key=key.pem --namespace observability
+
+rm cert.pem
+rm key.pem
+
+helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
+helm repo update
+helm install prometheus prometheus-community/prometheus --namespace observability
 
 helm repo add grafana https://grafana.github.io/helm-charts 
 helm repo update
-# helm pull grafana/grafana --untar
-helm upgrade grafana grafana/grafana --values values.yaml --install --atomic --debug --history-max=3 --namespace observability
+helm upgrade grafana grafana/grafana --values grafana-values.yaml --install --atomic --debug --history-max=3 --namespace observability
+
+git restore grafana-values.yaml
